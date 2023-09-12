4558bb22cd663d094fb68870ff602acdbf334373
Abhishek Chamoli <104618483+AbhishekChamoliDeveloper@users.noreply.github.com>
2023-09-12 20:38:37 +0530

    Algorithm Changed

--- videoEncryptor.js
+++ videoEncryptor.js
@@ -1,73 +1,55 @@
 const fs = require("fs");
+const crypto = require("crypto");
 const path = require("path");
-const crypto = require("crypto-js");
 
 class VideoEncryptor {
   constructor() {}
 
   encryptVideo(videoPath, encryptionKey, outputPath) {
-    try {
-      if (!fs.existsSync(videoPath)) {
-        throw new Error(`Video file does not exist at ${videoPath}`);
-      }
-
-      const videoBuffer = fs.readFileSync(videoPath);
-      const videoString = videoBuffer.toString("base64");
-      const encryptedString = crypto.AES.encrypt(
-        videoString,
-        encryptionKey
-      ).toString();
-
-      outputPath = this.replaceExtension(outputPath, "encrypted");
-
-      fs.writeFileSync(outputPath, encryptedString);
-      console.log(`Video encrypted and saved as ${outputPath}`);
-    } catch (error) {
-      console.error(`Error during video encryption: ${error.message}`);
+    if (!fs.existsSync(videoPath)) {
+      throw new Error(`Video file does not exist at ${videoPath}`);
     }
+
+    const videoBytes = fs.readFileSync(videoPath);
+
+    const cipher = crypto.createCipher("aes-256-cbc", encryptionKey);
+    let encryptedBytes = Buffer.concat([
+      cipher.update(videoBytes),
+      cipher.final(),
+    ]);
+
+    outputPath = this.replaceExtension(outputPath, "encrypted");
+
+    fs.writeFileSync(outputPath, encryptedBytes);
+
+    return `Video encrypted and saved as ${outputPath}`;
   }
 
-  decryptVideo(encryptedFilePath, encryptionKey, outputPath) {
-    try {
-      if (!fs.existsSync(encryptedFilePath)) {
-        throw new Error(
-          `Encrypted video file does not exist at ${encryptedFilePath}`
-        );
-      }
-
-      const encryptedString = fs.readFileSync(encryptedFilePath, "utf8");
-      const decryptedBytes = crypto.AES.decrypt(encryptedString, encryptionKey);
-
-      if (!decryptedBytes.sigBytes) {
-        throw new Error("Invalid decryption key or corrupted encrypted file.");
-      }
-
-      const videoBuffer = Buffer.from(
-        decryptedBytes.toString(crypto.enc.Utf8),
-        "base64"
+  decryptVideo(encryptedVideoPath, decryptionKey, outputPath) {
+    if (!fs.existsSync(encryptedVideoPath)) {
+      throw new Error(
+        `Encrypted video file does not exist at ${encryptedVideoPath}`
       );
-
-      if (!path.extname(outputPath)) {
-        outputPath = `${outputPath}.mp4`;
-      }
-
-      if (fs.existsSync(outputPath)) {
-        throw new Error(
-          `Output file already exists at ${outputPath}. Choose a different path.`
-        );
-      }
-
-      fs.writeFileSync(outputPath, videoBuffer);
-      console.log(`Video decrypted and saved as ${outputPath}`);
-    } catch (error) {
-      console.error(`Error during video decryption: ${error.message}`);
     }
+
+    const encryptedBytes = fs.readFileSync(encryptedVideoPath);
+
+    const decipher = crypto.createDecipher("aes-256-cbc", decryptionKey);
+    let decryptedBytes = Buffer.concat([
+      decipher.update(encryptedBytes),
+      decipher.final(),
+    ]);
+
+    outputPath = this.replaceExtension(outputPath, "mp4");
+
+    fs.writeFileSync(outputPath, decryptedBytes);
+
+    return `Video decrypted and saved as ${outputPath}`;
   }
 
   replaceExtension(filePath, newExtension) {
-    const currentExtension = path.extname(filePath);
-    const filenameWithoutExtension = path.basename(filePath, currentExtension);
-    return `${filenameWithoutExtension}.${newExtension}`;
+    const { dir, name } = path.parse(filePath);
+    return path.join(dir, `${name}.${newExtension}`);
   }
 }
 
